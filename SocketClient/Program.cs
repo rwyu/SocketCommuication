a51ade04e1846e2c0889dde389321eed71ea0258
yurenwei <806553981@qq.com>
2022-05-05 10:58:38 +0800

    推送全部代码

---
+++ SocketClient/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace superSocketClient
+{
+    class Program
+    {
+        static Socket socketClient { get; set; }
+        static void Main(string[] args)
+        {
+            //创建实例
+            socketClient = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ip = IPAddress.Parse("192.168.12.148");
+            IPEndPoint point = new IPEndPoint(ip, 2012);
+            //进行连接
+            socketClient.Connect(point);
+
+
+            //接收服务器端发送的消息
+            Thread thread = new Thread(Recive);
+            thread.IsBackground = true;
+            thread.Start();
+
+
+            //给服务器发送数据
+            Thread thread2 = new Thread(Send);
+            thread2.IsBackground = true;
+            thread2.Start();
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 接收消息
+        /// </summary>
+        /// <param name="o"></param>
+        static void Recive()
+        {
+            //  为什么用telnet客户端可以，但这个就不行。
+            while (true)
+            {
+                //获取发送过来的消息
+                byte[] buffer = new byte[1024 * 1024 * 2];
+                var effective = socketClient.Receive(buffer);
+                if (effective == 0)
+                {
+                    break;
+                }
+                var str = Encoding.UTF8.GetString(buffer, 0, effective);
+                Console.WriteLine("来自服务器 --- " + str);
+                Thread.Sleep(2000);
+            }
+        }
+
+
+        static void Send()
+        {      
+            int i=1;
+            while (true)
+            {
+                string strSend;
+                if (Console.ReadLine()!=String.Empty)
+                {
+                    strSend = Console.ReadLine();
+                } 
+                var buffter = Encoding.UTF8.GetBytes($"客户端:发送的第{i}条消息" + "\r\n");
+                var temp = socketClient.Send(buffter);
+                i++;
+                Thread.Sleep(10000);
+            }
+
+        }
+    }
+}
---
+++ SocketServer/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Protocol;
+
+namespace superSocketServer
+{
+    class Program
+    {
+        static AppServer appServer { get; set; }
+        static void Main(string[] args)
+        {
+            appServer = new AppServer();
+
+
+            //Setup the appServer
+            if (!appServer.Setup(2012)) //Setup with listening port
+            {
+                Console.WriteLine("Failed to setup!");
+                Console.ReadKey();
+                return;
+            }
+
+            //Try to start the appServer
+            if (!appServer.Start())
+            {
+                Console.WriteLine("Failed to start!");
+                Console.ReadKey();
+                return;
+            }
+
+
+            Console.WriteLine("The server started successfully, press key 'q' to stop it!");
+
+            //1.
+            appServer.NewSessionConnected += new SessionHandler<AppSession>(appServer_NewSessionConnected);
+            appServer.SessionClosed += appServer_NewSessionClosed;
+
+            //2.
+            appServer.NewRequestReceived += new RequestHandler<AppSession, StringRequestInfo>(appServer_NewRequestReceived);
+            
+            while (Console.ReadKey().KeyChar != 'q')
+            {
+                Console.WriteLine();
+                continue;
+            }
+
+            //Stop the appServer
+            appServer.Stop();
+
+            Console.WriteLine("The server was stopped!");
+            Console.ReadKey();
+        }
+
+        //1.
+        static void appServer_NewSessionConnected(AppSession session)
+        {
+            Console.WriteLine($"服务端得到来自客户端的连接成功");
+
+            var count = appServer.GetAllSessions().Count();
+            Console.WriteLine("~~" + count);
+            session.Send("Welcome to SuperSocket Telnet Server");
+        }
+
+        static void appServer_NewSessionClosed(AppSession session, CloseReason aaa)
+        {
+            Console.WriteLine($"服务端 失去 来自客户端的连接" + session.SessionID + aaa.ToString());
+            var count = appServer.GetAllSessions().Count();
+            Console.WriteLine(count);
+        }
+
+        //2.
+        static void appServer_NewRequestReceived(AppSession session, StringRequestInfo requestInfo)
+        {
+            Console.WriteLine(requestInfo.Key);
+            session.Send(requestInfo.Body);
+        }
+
+
+
+    }
+}
