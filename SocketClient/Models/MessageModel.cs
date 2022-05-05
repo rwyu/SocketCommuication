becf3cb1036753c11b43f3aa88f8827dc59b36d9
yurenwei <806553981@qq.com>
2022-05-05 12:32:04 +0800

    添加Models文件夹

---
+++ SocketClient/Models/MessageModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketClient.Models
+{
+    public class MessageModel
+    {
+        public string Name { get; set; }
+
+        public string StrMsg { get; set; }
+
+        public string StrSendTime { get { return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"); } }
+    }
+}
